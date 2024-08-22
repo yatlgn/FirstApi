dd1fa3ab63052b3b89d1caab7a8f9cdcf3d60e1b
Yaren Atılgan <yaren.atilgan@tedu.edu.tr>
2024-08-22 12:49:11 +0300

    CQRS yapıldı.

---
+++ Core/Api.Application/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application
+{
+    public static class Registration
+    {
+        public static void AddApplication(this IServiceCollection servises)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            servises.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+        }
+    }
+}
---
+++ Presentation/Api.Api/Controllers/CoachController.cs
@@ -0,0 +1,29 @@
+﻿using Api.Application.Features.Queries.GetAllCoach;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CoachController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public CoachController(IMediator mediator)
+        {
+            this.mediator = mediator;
+            
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> GetAllCoach()
+        {
+            var response = await mediator.Send(new GetAllCoachQueryRequest());
+
+            return Ok(response);
+        }
+
+
+    }
+}
